22244917ac6e20e676161bba928f46fec5536428
Shashwat <shashwat.pandey@utexas.edu>
2019-11-02 21:41:23 -0500

    Added goldman API request script

---
+++ gs.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+auth_data = {
+    "grant_type"    : "client_credentials",
+    "client_id"     : "be2457a2c0304ea6a0a046edd3df0dce",
+    "client_secret" : "e7ffb80965e1bf787992a9c50a8a4d6118545bcd6011b175cf569b889ebb143f",
+    "scope"         : "read_user_profile"
+}
+
+session = requests.Session()
+
+auth_request = session.post("https://idfs.gs.com/as/token.oauth2", data = auth_data)
+access_token_dict = json.loads(auth_request.text)
+access_token = access_token_dict["access_token"]
+
+# update session headers with access token
+session.headers.update({"Authorization":"Bearer "+ access_token})
+
+request_url = "https://api.marquee.gs.com/v1/groups"
+
+
+###Use this request query to poll data.
+# request_query = {
+#                     "where": {
+#                         "gsid": ["75154", "193067", "194688", "902608", "85627"]
+#                     },
+#                     "startDate": "2017-01-15",
+#                     "endDate":"2018-01-15"
+#                }
+# request = session.post(url=request_url, json=request_query)
+
+request = session.post(url=request_url)
+results = json.loads(request.text)
+
+print(results)
